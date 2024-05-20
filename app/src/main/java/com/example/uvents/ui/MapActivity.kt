a30c00f868c958228c5521198a7dc3418a40ad5f
m318405 <m318405@alunos.ipb.pt>
2024-05-20 10:00:59 +0200

    Refactor MapActivity in package ui

--- app/src/main/java/com/example/uvents/ui/MapActivity.kt
+++ app/src/main/java/com/example/uvents/ui/MapActivity.kt
@@ -1,4 +1,4 @@
-package com.example.uvents
+package com.example.uvents.ui
 
 import android.os.Bundle
 import android.widget.TextView
@@ -6,6 +6,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.uvents.R
 
 class MapActivity : AppCompatActivity() {
 
