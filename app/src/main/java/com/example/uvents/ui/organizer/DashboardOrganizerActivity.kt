e373773d61904e53ea90fc207680260cf6ad26c1
m318405 <m318405@alunos.ipb.pt>
2024-05-21 12:15:52 +0200

    dashoboard organizer view fixed with name and email showed

--- app/src/main/java/com/example/uvents/ui/organizer/DashboardOrganizerActivity.kt
+++ app/src/main/java/com/example/uvents/ui/organizer/DashboardOrganizerActivity.kt
@@ -1,6 +1,7 @@
 package com.example.uvents.ui.organizer
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,14 +9,24 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.uvents.R
 
 class DashboardOrganizerActivity : AppCompatActivity() {
+
+    private lateinit var etEmail: EditText
+    private lateinit var etCompanyName: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_dashboard_organizer)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.dashboard)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        etEmail = findViewById(R.id.etEmail)
+        etCompanyName = findViewById(R.id.etCompanyName)
+
+        etEmail.setText(intent.getStringExtra("EMAIL"))
+        etCompanyName.setText(intent.getStringExtra("NAME"))
     }
 }
